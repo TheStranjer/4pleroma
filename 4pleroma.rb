619e93d88a7c525088ce8d26b1ae29dd3215f99b
The Stranjer <791672+TheStranjer@users.noreply.github.com>
2021-03-09 15:24:52 -0700

    First draft

---
+++ 4pleroma.rb
@@ -0,0 +1,117 @@
+require 'json'
+require 'net/http'
+
+class FourPleroma
+  attr_accessor :info, :old_threads, :bearer_token, :instance, :filename, :skip_first
+
+  def initialize(fn)
+    @filename = fn
+    @info = JSON.parse(File.open(filename, "r").read)
+    @bearer_token = info['bearer_token']
+    @instance = info['instance']
+    @old_threads = []
+    @skip_first = true
+  end
+
+  def start
+    puts "4pleroma bot, watching catalog: #{info['catalog_url']}"
+
+    info["threads_touched"] ||= {}
+
+    while true
+      timestamp = Time.now.to_i
+
+      threads = JSON.parse(Net::HTTP.get(URI(info['catalog_url'])))
+      threads.collect! { |x| x["threads"] }
+      threads.flatten!
+
+      new_threads = {}
+
+      otn = thread_numbers(old_threads)
+      ntn = thread_numbers(threads)
+
+      deleted_thread_numbers = otn - ntn
+
+      threads.reject! { |x| deleted_thread_numbers.include?(x["no"]) }
+
+      info["threads_touched"].reject! { |k, v| deleted_thread_numbers.include?(k) }
+      info["old_threads"] = threads
+
+      threads.each do |thread|
+        thread_no = thread["no"].to_s
+        next if info["threads_touched"].keys.include?(thread_no) and info["threads_touched"][thread_no] >= thread["last_modified"]
+        thread_url = info['thread_url'].gsub("%%NUMBER%%", thread_no.to_s)
+        puts "EXAMINING THREAD: #{thread_url}"
+        JSON.parse(Net::HTTP.get(URI(thread_url)))["posts"].select { |x| x["time"] >= info["threads_touched"][thread_no].to_i and x["tim"] }.each do |post|
+          post_image(info['image_url'].gsub("%%TIM%%", post["tim"].to_s).gsub("%%EXT%%", post["ext"]), post)
+        end
+
+        info["threads_touched"][thread_no] = timestamp.to_i
+      end
+
+      f = File.open(filename, "w")
+      f.write(JSON.pretty_generate(info))
+      f.close
+
+      puts "SLEEPING NOW"
+
+      @skip_first = false
+      sleep 60
+    end
+  end
+
+  def post_image(url, post)
+    puts "\tFOUND NEW IMAGE: #{url}"
+    return if @skip_first
+    img = Net::HTTP.get(URI(url))
+
+    uri = URI.parse("https://#{instance}/api/v1/media")
+    header = {'Authorization': "Bearer #{bearer_token}"}
+
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+
+    f = File.open("temp", "w")
+    f.write(img)
+    f.close
+
+    req = Net::HTTP::Post.new(uri.request_uri)
+    req['Authorization'] = "Bearer #{bearer_token}"
+    req.set_form({"file" => File.open("temp")}, "multipart/form-data")
+
+    res = http.request(req)
+    response = JSON.parse(res.body)
+    
+    uri = URI.parse("https://#{@instance}/api/v1/statuses")
+    header = {
+      'Authorization'=> "Bearer #{bearer_token}",
+      'Content-Type' => 'application/json'
+    }
+
+    req = Net::HTTP::Post.new(uri.request_uri, header)
+    req.body = {
+      'status'       => "#{info['content_prepend']}#{process_html(post['com'])}#{info['content_append']}",
+      'source'       => '4pleroma',
+      'visibility'   => 'public',
+      'content_type' => 'text/html',
+      'media_ids'    => [response['id']]
+    }.to_json
+
+    puts req.body
+
+    res = http.request(req)
+  end
+
+  def process_html(html)
+    return "" if html.nil?
+    html.gsub!(/<a href="#p\d+" class="quotelink">&gt;&gt;\d+<\/a>/, "")
+    html.gsub!(/^&gt;(.+)$/, "<font color='#789922'>&gt;\\1</font>")
+    html
+  end
+
+  def thread_numbers(threads)
+    threads.collect { |x| x["no"] }
+  end
+end
+
+FourPleroma.new(ARGV.find { |x| /\.json$/i.match(x) } || "info.json").start
